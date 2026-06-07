8d8b1ff5c472d5d2ef10e346ac9f697838db9fef
nandanabinu2020-source <nandanabinu2020@gmail.com>
2026-06-07 13:24:17 +0530

    Update config.h

--- Task 04/sketches/day-09-ultrasonic/config.h
+++ Task 04/sketches/day-09-ultrasonic/config.h
@@ -1,1 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// HC-SR04 Pins
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+
+// Serial Configuration
+#define BAUD_RATE 9600
+
+// Maximum pulse timeout (microseconds)
+#define ULTRASONIC_TIMEOUT 30000
+
+#endif
